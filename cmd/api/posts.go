3eccf153fc32e84c3807becadfc62a84fd4709e7
Reza Heydari <mrreza1394@gmail.com>
2025-01-23 23:02:32 +0330

    Add validators

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -9,8 +9,8 @@ import (
 )
 
 type CreatePostPayload struct {
-	Title   string   `json:"title"`
-	Content string   `json:"content"`
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
 	Tags    []string `json:"tags"`
 }
 
@@ -19,6 +19,10 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	if err := readJSON(w, r, &payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
 	}
 
 	post := &store.Post{
