dde12656026e1c9a1656f5beda58e8d88244d174
Reza Heydari <mrreza1394@gmail.com>
2025-03-11 21:04:53 +0330

    add pagination

---
+++ cmd/api/feed.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/theycallmereza/social-app/internal/store"
+	"net/http"
+)
+
+func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
+	fq := store.PaginatedFeedQuery{
+		Limit:  10,
+		Offset: 0,
+		Sort:   "desc",
+	}
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(102), fq)
+	if err != nil {
+		app.internalServerError(w, r, err)
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, feed); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"github.com/lib/pq"
 )
 
@@ -17,6 +18,12 @@ type Post struct {
 	UpdatedAt string    `json:"updated_at"`
 	Version   int       `json:"version"`
 	Comments  []Comment `json:"comments"`
+	User      User      `json:"user"`
+}
+
+type PostWithMetadata struct {
+	Post
+	CommentsCount int `json:"comments_count"`
 }
 
 type PostStore struct {
@@ -127,4 +134,62 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 		}
 	}
 	return nil
+}
+
+func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]PostWithMetadata, error) {
+	// Validate Sort Input
+	sortOrder := "DESC"
+	if fq.Sort == "ASC" {
+		sortOrder = "ASC"
+	}
+
+	query := fmt.Sprintf(`
+		SELECT p.id,
+			   p.user_id,
+			   p.title,
+			   p.content,
+			   p.created_at,
+			   p.version,
+			   p.tags,
+			   u.username,
+			   (SELECT COUNT(*) FROM comments WHERE post_id = p.id) AS comments_count
+		FROM posts p
+		LEFT JOIN users u ON p.user_id = u.id
+		LEFT JOIN followers f ON f.user_id = p.user_id
+		WHERE f.follower_id = $1 OR p.user_id = $1
+		GROUP BY p.id, u.username, p.created_at
+		ORDER BY p.created_at %s
+		LIMIT $2 OFFSET $3
+	`, sortOrder)
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute query: %w", err)
+	}
+	defer rows.Close()
+
+	var feed []PostWithMetadata
+	for rows.Next() {
+		var p PostWithMetadata
+		err := rows.Scan(
+			&p.Post.ID,
+			&p.Post.UserID,
+			&p.Post.Title,
+			&p.Post.Content,
+			&p.Post.CreatedAt,
+			&p.Post.Version,
+			pq.Array(&p.Tags),
+			&p.User.Username,
+			&p.CommentsCount,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan row: %w", err)
+		}
+		feed = append(feed, p)
+	}
+
+	return feed, nil
 }
